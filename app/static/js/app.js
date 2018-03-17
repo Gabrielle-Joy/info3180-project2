4a393ec2135d6b19bdf3d67c0ace6bcb649a3a58
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2018-03-17 12:21:10 -0500

    Add initial header and footer components

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,7 +1,45 @@
 /* Add your Application JavaScript */
+Vue.component('app-header', {
+    template: `
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+              <a class="navbar-brand" href="#">VueJS App</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+        </header>    
+    `,
+    data: function() {}
+});
+
+Vue.component('app-footer', {
+    template: `
+        <footer>
+            <div class="container">
+                <p>Copyright &copy; Flask Inc.</p>
+            </div>
+        </footer>
+    `,
+    data: function() {}
+})
+
+
 let app = new Vue({
     el: '#app',
     data: {
         welcome: 'Hello World! Welcome to VueJS'
     }
 });
+
