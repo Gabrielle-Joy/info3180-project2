d2a0c1815a5a2a9437bf07deb44c06a30034b07e
NateChris <divineconclusion@gmail.com>
2020-05-13 15:06:00 -0500

    Differentiate btw images for photogram and uploads

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "the best com-sci gods MAX POWAH"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://proj2-user:unlocked@localhost/info3180-proj2"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
-app.config['UPLOAD_FOLDER'] = './app/static/images'
+app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 
 db = SQLAlchemy(app)
 
