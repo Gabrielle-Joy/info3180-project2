56d6ff51ff003f1f97ac7a8051ee9bb31d8920dc
Gabrielle-Joy <gabzi.contact@gmail.com>
2020-05-16 13:46:01 -0500

    header

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,36 +1,36 @@
 /* Add your Application JavaScript */
 Vue.component('app-header', {
-    template: `
-        <header>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-              <a class="navbar-brand" href="/">
-                <img src="../static/images/interface.png" width="30" height="30" class="d-inline-block align-top" alt="mkk"/>
-                Photogram
-              </a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
+  template: `
+      <header>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+            <a class="navbar-brand" href="/">
+              <img src="../static/images/interface.png" class="nav-logo d-inline-block align-top" alt="mkk"/>
+              Photogram
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
 
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="/explore">Explore</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="/users/:user_id">My Profile</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="/logout">Logout</a>
-                  </li>
-                </ul>
-                </ul>
-              </div>
-            </nav>
-        </header>    
-    `,
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/explore">Explore</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/users/:user_id">My Profile</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/logout">Logout</a>
+                </li>
+              </ul>
+              </ul>
+            </div>
+          </nav>
+      </header>    
+  `,
     data: function() {
       return {};
     }
