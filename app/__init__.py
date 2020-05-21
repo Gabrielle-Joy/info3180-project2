795605d600d3729d2b298743fcba2cb6116bd719
Rowan Atkinson <rowaneatkinson@gmail.com>
2020-05-21 08:57:26 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://proj2-user:unlocked@localhost/info3180-proj2"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://jcrcdvvolvnzkc:e4c335058dfb5ff226421b092e5ac7fd5ebbb2411fdcbdbf48cbf56203f09129@ec2-54-81-37-115.compute-1.amazonaws.com:5432/dbf8o01ugtvcj9"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://jcrcdvvolvnzkc:e4c335058dfb5ff226421b092e5ac7fd5ebbb2411fdcbdbf48cbf56203f09129@ec2-54-81-37-115.compute-1.amazonaws.com:5432/dbf8o01ugtvcj9"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 
